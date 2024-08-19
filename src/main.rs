f4baffab4ecf62aadc6e272be7984b0d0291809a
Nick Pfundstein <nickpfundstein@gmail.com>
2024-08-19 11:11:38 -0600

    Add fancy scene

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use crate::bvh::Node;
 use crate::camera::Camera;
-use crate::hittable::{ConstantMedium, Hittable, Quad, RotateY, Sphere, Translate};
+use crate::hittable::{make_box, ConstantMedium, Hittable, Quad, RotateY, Sphere, Translate};
 use crate::material::{Dielectric, DiffuseLight, Isotropic, Lambertian, Metal};
 use crate::texture::{Checker, Image, Noise, SolidColor};
 use crate::vec3::Vec3;
@@ -40,6 +40,8 @@ fn main() {
         "simple-light" => simple_light(),
         "cornell" => cornell_box(),
         "cornell-smoke" => cornell_smoke(),
+        "fancy-full" => fancy(800, 10000, 40),
+        "fancy-light" => fancy(400, 250, 4),
         _ => panic!("unknown scene"),
     };
 
@@ -604,6 +606,148 @@ fn cornell_smoke() -> RgbImage {
         50,
         40.0,
         Vec3([278.0, 278.0, -800.0]),
+        Vec3([278.0, 278.0, 0.0]),
+        Vec3([0.0, 1.0, 0.0]),
+        0.0,
+        10.0,
+        Vec3([0.0, 0.0, 0.0]),
+    );
+
+    camera.render(&world)
+}
+
+fn fancy(image_width: u32, samples: u32, max_depth: u32) -> RgbImage {
+    let mut rand = rand::thread_rng();
+
+    let ground = Lambertian {
+        texture: SolidColor::new(Vec3([0.48, 0.83, 0.53])),
+    };
+
+    let mut boxes: Vec<Box<dyn Hittable>> = Vec::new();
+    for i in 0..20 {
+        for j in 0..20 {
+            let w = 100.0;
+            let x0 = -1000.0 + i as f64 * w;
+            let z0 = -1000.0 + j as f64 * w;
+            let y0 = 0.0;
+            let x1 = x0 + w;
+            let y1 = rand.gen::<f64>() * 100.0 + 1.0;
+            let z1 = z0 + w;
+
+            boxes.push(Box::new(make_box(
+                Vec3([x0, y0, z0]),
+                Vec3([x1, y1, z1]),
+                ground.clone(),
+            )));
+        }
+    }
+
+    let earth_texture = image::open("earthmap.jpg").unwrap().into_rgb8();
+
+    let mut world: Vec<Box<dyn Hittable>> = vec![
+        Box::new(Node::from_list(boxes)),
+        Box::new(Quad::new(
+            Vec3([123.0, 554.0, 147.0]),
+            Vec3([300.0, 0.0, 0.0]),
+            Vec3([0.0, 0.0, 265.0]),
+            DiffuseLight::new(SolidColor::new(Vec3([7.0, 7.0, 7.0]))),
+        )),
+        Box::new(Sphere::new(
+            Vec3([260.0, 150.0, 45.0]),
+            50.0,
+            Dielectric {
+                refraction_index: 1.5,
+            },
+        )),
+        Box::new(Sphere::new(
+            Vec3([0.0, 150.0, 145.0]),
+            50.0,
+            Metal {
+                albedo: Vec3([0.8, 0.8, 0.9]),
+                fuzz: 1.0,
+            },
+        )),
+        Box::new(Sphere::new(
+            Vec3([360.0, 150.0, 145.0]),
+            70.0,
+            Dielectric {
+                refraction_index: 1.5,
+            },
+        )),
+        Box::new(ConstantMedium::new(
+            Box::new(Sphere::new(
+                Vec3([360.0, 150.0, 145.0]),
+                70.0,
+                Dielectric {
+                    refraction_index: 1.5,
+                },
+            )),
+            0.2,
+            Isotropic::new(SolidColor::new(Vec3([0.2, 0.4, 0.9]))),
+        )),
+        Box::new(ConstantMedium::new(
+            Box::new(Sphere::new(
+                Vec3([0.0, 0.0, 0.0]),
+                5000.0,
+                Dielectric {
+                    refraction_index: 1.5,
+                },
+            )),
+            0.0001,
+            Isotropic::new(SolidColor::new(Vec3([1.0, 1.0, 1.0]))),
+        )),
+        Box::new(Sphere::new(
+            Vec3([400.0, 200.0, 400.0]),
+            100.0,
+            Lambertian {
+                texture: Image::new(earth_texture),
+            },
+        )),
+        Box::new(Sphere::new(
+            Vec3([220.0, 280.0, 300.0]),
+            80.0,
+            Lambertian {
+                texture: Noise::<256>::new(0.2),
+            },
+        )),
+    ];
+
+    let center1 = Vec3::scalar(400.0);
+    let center2 = center1 + Vec3([30.0, 0.0, 0.0]);
+    world.push(Box::new(Sphere::moving(
+        center1,
+        center2,
+        50.0,
+        Lambertian {
+            texture: SolidColor::new(Vec3([0.7, 0.3, 0.1])),
+        },
+    )));
+
+    let mut boxes2: Vec<Box<dyn Hittable>> = Vec::new();
+    for _ in 0..1000 {
+        boxes2.push(Box::new(Sphere::new(
+            Vec3::random_within(0.0, 165.0),
+            10.0,
+            Lambertian {
+                texture: SolidColor::new(Vec3::scalar(0.73)),
+            },
+        )));
+    }
+
+    world.push(Box::new(Translate::new(
+        Box::new(RotateY::new(Box::new(Node::from_list(boxes2)), 15.0)),
+        Vec3([-100.0, 270.0, 395.0]),
+    )));
+
+    let world = Node::from_list(world);
+
+    let camera = Camera::new(
+        1.0,
+        image_width,
+        samples,
+        max_depth,
+        40.0,
+        Vec3([478.0, 278.0, -600.0]),
         Vec3([278.0, 278.0, 0.0]),
         Vec3([0.0, 1.0, 0.0]),
         0.0,
